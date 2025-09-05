791aa11b0bb45ed7177d931f691939dbe39e598f
Joseph Wondimagnehu <joseph.wondimagnehu@stu.ocsb.ca>
2025-09-05 01:06:32 +0000

    Added HelloWorld.java.

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+* This program greets a friend and
+* outputs an additional statement.
+*
+* @author  Joseph Wondimagnehu
+* @version 1.0
+* @since   2025-09-04
+*/
+
+// Creates the container for the program.
+class HelloWorld {
+  // Defines the entry point of the program.
+  public static void main(String[] args) {
+    // Greets Beni.
+    System.out.println("\nHello, Beni!");
+    // Outputs an additional statement.
+    System.out.println("Java is the superior language! In my opinion, of course.\n");
+  }
+}
